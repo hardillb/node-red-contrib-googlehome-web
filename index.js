77c649fef502daec44c98d1ebc600687755356b1
Ben Hardill <hardillb@gmail.com>
2018-12-28 18:27:30 +0000

    Allow log level to be set externally
    export LOG_LEVEL=debug

--- index.js
+++ index.js
@@ -29,6 +29,9 @@ var loggingOptions = {
 
 var logger = SimpleNodeLogger.createRollingFileLogger(loggingOptions);
 
+var logLevel (process.env.LOG_LEVEL || "info");
+logger.setLevel(logLevel);
+
 var port = (process.env.VCAP_APP_PORT || process.env.PORT || 3000);
 var host = (process.env.VCAP_APP_HOST || '0.0.0.0');
 var mongo_url = (process.env.MONGO_URL || 'mongodb://localhost:27017/assistant');
